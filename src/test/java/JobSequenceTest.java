496361b7acfeac0be9cb8dcda5efd24fcd0a243d
Aditya Godara <aditya.godara@smartebusiness.co.uk>
2019-07-29 21:02:39 +0100

    initialize jobSequence object in before method for each test

--- src/test/java/JobSequenceTest.java
+++ src/test/java/JobSequenceTest.java
@@ -1,18 +1,26 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 public class JobSequenceTest {
+    private JobSequence jobSequence;
+
+    @Before
+    public void setup() {
+        jobSequence = new JobSequence();
+    }
+
     @Test
     public void givenNoJobsTheResultIsAnEmptySequence() {
-        JobSequence jobSequence = new JobSequence();
+        jobSequence = new JobSequence();
         String result = jobSequence.order("");
         assertThat(result, is(""));
     }
 
     @Test
     public void givenASingleJobTheResultConsistsOfASingleJob() {
-        JobSequence jobSequence = new JobSequence();
+        jobSequence = new JobSequence();
         String result = jobSequence.order("a =>");
         assertThat(result, is("a"));
     }
