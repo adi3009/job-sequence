15cf3c72dbbeea26e8452884c685827d5e9cdb31
Aditya Godara <aditya.godara@smartebusiness.co.uk>
2019-07-30 08:01:17 +0100

    sequence should not assigned again in each test method

--- src/test/java/job/SequenceTest.java
+++ src/test/java/job/SequenceTest.java
@@ -16,28 +16,24 @@ public class SequenceTest {
 
     @Test
     public void givenNoJobsTheResultIsAnEmptySequence() {
-        sequence = new Sequence();
         String result = sequence.order("");
         assertThat(result, is(""));
     }
 
     @Test
     public void givenASingleJobTheResultConsistsOfASingleJob() {
-        sequence = new Sequence();
         String result = sequence.order("a =>");
         assertThat(result, is("a"));
     }
 
     @Test
     public void givenIndependentJobsTheResultContainsJobsInNoParticularOrder() {
-        sequence = new Sequence();
         String result = sequence.order("a =>\nb =>\nc =>\n");
         assertThat(result, is("abc"));
     }
 
     @Test
     public void givenDependencyBetweenJobsTheResultContainsDependentJobsAfterIndependentJobs() {
-        sequence = new Sequence();
         String result = sequence.order("a =>\nb =>c\nc =>\n");
         assertThat(result, is("acb"));
         result = sequence.order("a =>\nb =>c\nc =>f\nd=>a\ne=>b\nf=>");
